9140770df534a1717c8845e252f2869a3e09dac1
abbse <aleix.badia@somenergia.coop>
2016-02-02 19:14:43 +0100

    Fix typos

--- get_ots/cli.py
+++ get_ots/cli.py
@@ -20,8 +20,8 @@ def get_ots_all_contracts():
 @emp.command()
 @click.argument('filename', type=click.Path(exists=True))
 @click.option('--ts', default=None)
-def get_ots_contracts(filename, ts):
-    get_ots_contracts_orig(list_from_file(filename), ts)
+def get_ots_contracts(filename, ts=None):
+    get_ots_contracts_orig(list_from_file(filename))
 
 if __name__ == '__main__':
     emp(obj={})
