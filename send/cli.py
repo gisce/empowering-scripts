7f9ef1335a77dd4539103b070081dadc4425582e
abbse <aleix.badia@somenergia.coop>
2016-02-19 11:33:23 +0100

    IMP Check whether data is available

--- send/cli.py
+++ send/cli.py
@@ -1,28 +1,17 @@
 import os
-import ooop
+import erppeek
+import ramman 
 import click
+import report
+import utils
 
 config = {
     'uri': os.getenv('PEEK_SERVER', None),
     'dbname': os.getenv('PEEK_DB', None),
     'user': os.getenv('PEEK_USER', None),
-    'password': os.getenv('PEEK_PASSWORD', None)
+    'password': os.getenv('PEEK_PASSWORD', None),
+    'heman': os.getenv('HEMAN_URL', None)
 }
-
-def list_from_file(path):
-    import csv
-
-    with open(path, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        return [row[0] for row in reader]
-
-def send(obj, contract_ids):
-    search_params = [('cups.empowering', '=', True),
-                     ('cups.empowering_quarantine', '=', 1)]
-    if contract_ids is not  None:
-        search_params.append(('name', 'in', contract_ids))
-    contract_ids = obj.GiscedataPolissa.search(search_params)
-    obj.GiscedataPolissa.send_empowering_report(contract_ids)
 
 @click.group()
 @click.pass_context
@@ -32,13 +21,16 @@ def erp(ctx):
     config = ctx.obj['config']
     try:
         url = urlparse(config['uri'])
-        ctx.obj['erp'] = ooop.OOOP(
-             uri='{scheme}://{hostname}'.format(**{'scheme':url.scheme, 'hostname':url.hostname}),
-             port=url.port,
-             dbname=config['dbname'],
-             user=config['user'],
-             pwd=config['password']
+        ctx.obj['erp'] = erppeek.Client( 
+             '{scheme}://{hostname}:{port}'.format(
+                                  **{'scheme':url.scheme,
+                                     'hostname':url.hostname,
+                                     'port':url.port}),
+             config['dbname'],
+             config['user'],
+             config['password']
              )
+        ctx.obj['heman'] = ramman.Ramman({'url': config['heman']})
     except Exception, ex:
         click.echo('ERP connection failed')
         click.echo(ex)
@@ -47,13 +39,13 @@ def erp(ctx):
 @erp.command()
 @click.pass_context
 @click.argument('filename', type=click.Path(exists=True))
-def send_report(ctx, filename):
-    send(ctx.obj['erp'], list_from_file(filename))
+def deliver(ctx, filename):
+    report.deliver(ctx.obj, utils.list_from_file(filename))
 
 @erp.command()
 @click.pass_context
-def send_all_report(ctx, filename):
-    send(ctx.obj['erp'], None)
+def deliver_all(ctx, filename):
+    report.deliver(ctx.obj, None)
 
 if __name__ == '__main__':
     erp(obj={'config': config})
